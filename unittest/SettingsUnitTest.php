b3a78a9d653163d2319f10200ca43d3b4d69450d
GeekWisdom <braddet@gmail.com>
2019-05-20 17:07:56 -0300

    Added GetSettingReverse

--- unittest/SettingsUnitTest.php
+++ unittest/SettingsUnitTest.php
@@ -1,6 +1,10 @@
-<? include (dirname(__FILE__) . "/../org/geekwisdom/settings/gwSettings.php");
+<? include (dirname(__FILE__) . "/../org/geekwisdom/settings/gwSettings.lib");
 $connectstr="mysql:host=192.168.0.15;dbname=braddb;charset=utf8;uid=adminbrad;pw=blc4fr";
-$mysm = new gwSettings();
-$r = $mysm->GetSetting($connectstr,"LogVerbosity","0");
-echo "R is $r";
+$mysm = new GWSettings();
+//$r = $mysm->GetSetting($connectstr,"LogVerbosity","0");
+$r = $mysm->GetSetting("./test.config","test","0");
+echo "R is $r\n";	
+$r = $mysm->GetSettingReverse("./test.config","Cool Dude","0");
+echo "R is $r\n";
+
 ?>
