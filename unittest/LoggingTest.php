b9393c26a4206557e7ee8600691382ed7c595bfc
GeekWisdom <braddet@gmail.com>
2019-07-01 10:45:25 -0300

    Reorg for Composer

--- unittest/LoggingTest.php
+++
@@ -1,5 +0,0 @@
-<?php include (dirname(__FILE__) . "/../org/geekwisdom/logging/gwLogger.lib");
-$mylog = new GWLogger(5,"main"); //loglevel 5
-$mylog->WriteLog(1,LogType::Warning,"This is a warning message","");
-$mylog->WriteLog(4,LogType::Info,"This is a information message","");
-?>
